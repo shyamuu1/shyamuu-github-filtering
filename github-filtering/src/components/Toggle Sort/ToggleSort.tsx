d75c703182faff75a2fcb91a293eb5e438e30197
shyam vasanjee <shyamuu.vasanjee@gmail.com>
2021-03-10 16:16:44 -0500

    created toggle sort switch component

---
+++ github-filtering/src/components/Toggle Sort/ToggleSort.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {FormGroup} from "@material-ui/core";
+
+const ToggleSort:React.FC = () => {
+    return (
+        <>
+        <FormGroup row>
+
+        </FormGroup>
+        </>
+    );
+};
+
+export default ToggleSort;
