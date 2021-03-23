6430a9b8884e055ab81b740dae7fa31d23ae2673
shyam vasanjee <shyamuu.vasanjee@gmail.com>
2021-03-23 19:31:09 -0400

    added modules for components and containers

---
+++ github-filtering/src/components/index.ts
@@ -0,0 +1,10 @@
+import Filters from "./Filters/Filters";
+import Layout from "./Layout/Layout";
+import Organizations from "./Organizations/Organizations";
+import Repolist from "./Repository List/Repolist";
+import Search from "./Search/Search";
+import ToggleSort from "./Toggle Sort/ToggleSort";
+import UserDetail from "./User Detail/UserDetail";
+import UserList from "./User Popular Repo List/UserList";
+
+export {Filters, Layout, Organizations, Repolist, Search, ToggleSort, UserDetail, UserList};
---
+++ github-filtering/src/containers/index.ts
@@ -0,0 +1,4 @@
+import SearchPage from "./Search Container/SearchPage";
+import SearchDetailPage from "./Search Detail/SearchDeatailPage";
+
+export {SearchPage, SearchDetailPage};
